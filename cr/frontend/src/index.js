104848f996266ef3dc7a124651f3b96ec916aef4
kekee000 <kekee000@gmail.com>
2018-01-12 10:34:18 +0800

    cr

---
+++ cr/frontend/src/index.js
@@ -0,0 +1,14 @@
+import 'normalize.css/normalize.css';
+import 'san-mui/index.css';
+import './common/component/ui-common.styl';
+
+import './home/index';
+import './scalars/index';
+import './images/index';
+
+import App from './App';
+new App({
+    data: {
+        titleName: 'VisualDL'
+    }
+}).attach(document.getElementById('root'));
